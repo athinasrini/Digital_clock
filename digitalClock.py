21d5cf870cc8ddccd129f877d1139f41130b5100
athinasrini <99618521+athinasrini@users.noreply.github.com>
2022-02-13 16:38:34 -0500

    Update digitalClock.py

--- digitalClock.py
+++ digitalClock.py
@@ -2,4 +2,7 @@ from tkinter import Tk, Label
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
+window.configure(bg="red")
+label = Label(window, text="SriniA",font=("Arial Black",78,"bold"),bg="red",fg="white")
+label.pack(pady=100)
 window.mainloop()
